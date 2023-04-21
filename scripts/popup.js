b79e571deec50b4f7ab3d1170e45a27eb84f15cd
James Kehs <jameskehsdev@gmail.com>
2023-04-20 22:52:51 -0500

    Styling Changes

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,24 +1,24 @@
 const copies = [];
-const createCopyBtn = document.getElementById("create-copy");
-const createCopyForm = document.getElementById("create-copy-form");
-const clearLocalStorageBtn = document.getElementById("clear-local");
-const paramsForm = document.getElementById("params-form");
-const seedCopiesBtn = document.getElementById("seed-copies");
-const closeParamsFormBtn = document.getElementById("close-params-form-btn");
+const createCopyBtn = document.getElementById('create-copy');
+const newCopyForm = document.getElementById('create-copy-form');
+const deleteAllCopiesBtn = document.getElementById('clear-local');
+const paramsForm = document.getElementById('params-form');
+const seedCopiesBtn = document.getElementById('seed-copies');
+const closeParamsFormBtn = document.getElementById('close-params-form-btn');
 const numberOfSeeds = 5;
 
-createCopyBtn.addEventListener("click", () => toggleForm());
-createCopyForm.addEventListener("submit", (e) => submitForm(e));
-clearLocalStorageBtn.addEventListener("click", () => clearLocalCopies());
-seedCopiesBtn.addEventListener("click", () => seedCopies(numberOfSeeds));
-closeParamsFormBtn.addEventListener("click", () => {
-  document.querySelectorAll(".param-field").forEach((e) => e.remove());
-  paramsForm.style.display = "none";
+createCopyBtn.addEventListener('click', () => toggleCreateForm());
+newCopyForm.addEventListener('submit', (e) => submitNewCopyForm(e));
+deleteAllCopiesBtn.addEventListener('click', () => clearLocalCopies());
+seedCopiesBtn.addEventListener('click', () => seedCopies(numberOfSeeds));
+closeParamsFormBtn.addEventListener('click', () => {
+  document.querySelectorAll('.param-field').forEach((e) => e.remove());
+  paramsForm.style.display = 'none';
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const storedCopies = JSON.parse(localStorage.getItem("EXT_COPIES"));
-  if (!storedCopies) localStorage.setItem("EXT_COPIES", "[]");
+document.addEventListener('DOMContentLoaded', () => {
+  const storedCopies = JSON.parse(localStorage.getItem('EXT_COPIES'));
+  if (!storedCopies) localStorage.setItem('EXT_COPIES', '[]');
   else if (storedCopies.length === 0) return;
   else {
     for (storedCopy of storedCopies) {
@@ -27,29 +27,32 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-function toggleForm() {
-  document.getElementById("create-copy-form").classList.toggle("active");
+function toggleCreateForm() {
+  document.getElementById('create-copy-form').classList.toggle('active');
 
-  createCopyBtn.classList.toggle("cancel-action");
-  if (createCopyBtn.classList.contains("cancel-action")) {
-    createCopyBtn.innerText = "Cancel";
+  createCopyBtn.classList.toggle('cancel-action');
+  if (createCopyBtn.classList.contains('cancel-action')) {
+    createCopyBtn.innerText = 'Cancel';
+    createCopyBtn.classList.add('destructive-action-btn');
   } else {
-    createCopyBtn.innerText = "Add Copy";
+    createCopyBtn.innerText = 'Add Copy';
+    createCopyBtn.classList.remove('destructive-action-btn');
   }
 }
-function submitForm(e) {
+function submitNewCopyForm(e) {
   e.preventDefault();
-  const title = document.getElementById("create-copy-name").value;
-  const value = tinymce.get("create-copy-textarea").getContent();
+  const title = document.getElementById('create-copy-name').value;
+  const value = tinymce.get('create-copy-textarea').getContent();
   createCopy({ title, value });
-  toggleForm();
+  toggleCreateForm();
 }
 
 function createCopy(newCopy) {
   let { id, title, value, addToLocalStorage = true } = newCopy;
-  const newDiv = document.createElement("div");
-  const copyTitle = document.createElement("h1");
-  const copyContents = document.createElement("div");
+  const newDiv = document.createElement('div');
+  const copyTitle = document.createElement('h1');
+  const copyContents = document.createElement('div');
+  console.log(value);
   const params = value.match(/(?<=\[\[).*?(?=\]\])/g) || [];
 
   if (!id) id = `COPY_${new Date().getTime()}`;
@@ -57,36 +60,36 @@ function createCopy(newCopy) {
   copyContents.innerHTML = value;
 
   newDiv.id = id;
-  copyTitle.classList.add("copy-title");
-  copyContents.classList.add("copy-value");
-  newDiv.classList.add("copy");
+  copyTitle.classList.add('copy-title');
+  copyContents.classList.add('copy-value');
+  newDiv.classList.add('copy');
 
   newDiv.appendChild(copyTitle);
   newDiv.appendChild(copyContents);
-  newDiv.addEventListener("click", (e) => copyValue(newDiv.id, e));
+  newDiv.addEventListener('click', (e) => copyValue(newDiv.id, e));
 
-  document.getElementById("all-copies").append(newDiv);
+  document.getElementById('all-copies').append(newDiv);
 
   if (addToLocalStorage) {
-    const existingCopies = JSON.parse(localStorage.getItem("EXT_COPIES"));
+    const existingCopies = JSON.parse(localStorage.getItem('EXT_COPIES'));
     existingCopies.push({ id, title, value, params });
-    localStorage.setItem("EXT_COPIES", JSON.stringify(existingCopies));
-    document.getElementById("create-copy-name").value = "";
-    tinymce.get("create-copy-textarea").setContent("");
+    localStorage.setItem('EXT_COPIES', JSON.stringify(existingCopies));
+    document.getElementById('create-copy-name').value = '';
+    tinymce.get('create-copy-textarea').setContent('');
   }
 }
 
 function copyValue(id) {
-  const allCopies = JSON.parse(localStorage.getItem("EXT_COPIES"));
+  const allCopies = JSON.parse(localStorage.getItem('EXT_COPIES'));
   const thisCopy = allCopies.filter((copy) => copy.id === id)[0];
   if (thisCopy.params.length > 0) enableEditParams(thisCopy);
   else {
     const copy = document.querySelector(`#${id}`);
     const copyContents = document.querySelector(`#${id} div.copy-value`).textContent;
     navigator.clipboard.writeText(copyContents);
-    copy.classList.add("copied");
+    copy.classList.add('copied');
     setTimeout(() => {
-      copy.classList.remove("copied");
+      copy.classList.remove('copied');
     }, 500);
   }
 }
@@ -95,16 +98,17 @@ function enableEditParams(copy) {
   const copyDiv = document.querySelector(`#${copy.id}`);
   const copyContents = document.querySelector(`#${copy.id} div.copy-value`).textContent;
 
-  paramsForm.style.display = "block";
+  paramsForm.style.display = 'block';
   for (param of copy.params.reverse()) {
-    const newDiv = document.createElement("div");
-    const newLabel = document.createElement("label");
-    const newInput = document.createElement("input");
+    const newDiv = document.createElement('div');
+    const newLabel = document.createElement('label');
+    const newInput = document.createElement('input');
 
-    newDiv.classList.add("param-field");
+    newDiv.classList.add('param-field');
     newLabel.innerText = param;
     newInput.name = param;
-    newInput.placeholder = "Param Value";
+    newInput.placeholder = 'Param Value';
+    newInput.required = true;
 
     newDiv.appendChild(newLabel);
     newDiv.appendChild(newInput);
@@ -112,28 +116,28 @@ function enableEditParams(copy) {
     paramsForm.prepend(newDiv);
   }
 
-  paramsForm.addEventListener("submit", (e) => {
+  paramsForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const allInputs = document.querySelectorAll(".param-field input");
+    const allInputs = document.querySelectorAll('.param-field input');
     let alteredcopy = copyContents;
     for (input of allInputs) {
-      const regex = new RegExp(`\\[\\[(${input.name})\\]\\]`, "gi");
+      const regex = new RegExp(`\\[\\[(${input.name})\\]\\]`, 'gi');
       alteredcopy = alteredcopy.replace(regex, input.value);
     }
     navigator.clipboard.writeText(alteredcopy);
-    document.querySelectorAll(".param-field").forEach((e) => e.remove());
-    paramsForm.style.display = "none";
+    document.querySelectorAll('.param-field').forEach((e) => e.remove());
+    paramsForm.style.display = 'none';
 
-    copyDiv.classList.add("copied");
+    copyDiv.classList.add('copied');
     setTimeout(() => {
-      copyDiv.classList.remove("copied");
+      copyDiv.classList.remove('copied');
     }, 500);
   });
 }
 
 function clearLocalCopies() {
-  localStorage.setItem("EXT_COPIES", "[]");
-  const allCopies = document.getElementById("all-copies");
+  localStorage.setItem('EXT_COPIES', '[]');
+  const allCopies = document.getElementById('all-copies');
   while (allCopies.firstChild) {
     allCopies.removeChild(allCopies.lastChild);
   }
@@ -141,8 +145,8 @@ function clearLocalCopies() {
 function seedCopies(numberOfCopies) {
   (function myLoop(i) {
     const newCopy = {
-      title: "Test",
-      value: "<p>This is a [[test]]</p>",
+      title: 'Test',
+      value: '<p>This is a [[test]]</p>',
       params: [],
     };
     setTimeout(function () {
@@ -150,4 +154,11 @@ function seedCopies(numberOfCopies) {
       if (--i) myLoop(i);
     }, 100);
   })(numberOfCopies);
+}
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+if (JSON.parse(urlParams.get('dev'))) {
+  document.getElementById('seed-copies').style.display = 'initial';
 }
